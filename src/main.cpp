8c4b5aca1bf1aca9beb838fa9b215d81b958f945
Conan Bitter <conan.bitter@gmail.com>
2025-03-03 19:19:03 +0300

    Scaffold SDL application

---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
+const int SCREEN_WIDTH = 320;   // 640
+const int SCREEN_HEIGHT = 180;  // 480
+const int SCREEN_SCALE = 4;
+
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Texture *screen;
+SDL_Rect screen_rect = {0, 0, SCREEN_WIDTH *SCREEN_SCALE, SCREEN_HEIGHT *SCREEN_SCALE};
+
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
+    window = SDL_CreateWindow(
+        "Murom",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH * SCREEN_SCALE,
+        SCREEN_HEIGHT * SCREEN_SCALE,
+        SDL_WINDOW_RESIZABLE);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    screen = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    SDL_Event event;
+    bool working = true;
+
+    while (working) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    working = 0;
+                    break;
+            }
+        }
+
+        SDL_RenderCopy(renderer, screen, NULL, &screen_rect);
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(5);
+    }
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
